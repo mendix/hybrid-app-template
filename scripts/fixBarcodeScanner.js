745d373c4dbc59f72b4980c98562cbfa18306c65
Diego Antonelli <diego.antonelli@mendix.com>
2023-04-26 15:10:29 +0200

    Fix barcode scanner in Android

--- scripts/fixBarcodeScanner.js
+++ scripts/fixBarcodeScanner.js
@@ -1,9 +1,15 @@
-const {existsSync, readFileSync, writeFileSync} = require("fs");
+const {existsSync, readFileSync, writeFileSync, readdirSync} = require("fs");
 const {join} = require("path");
-const location = join("build/platforms/android/phonegap-plugin-barcodescanner", "*.gradle");
+const location = join("build/platforms/android/phonegap-plugin-barcodescanner");
 
 if (existsSync(location)) {
-    const fileContent = readFileSync(location, "utf-8");
-    // Adds compatibility to new gradle 7
-    writeFileSync(location, fileContent.replace("compile(", "implementation("));
+    const files = readdirSync(location);
+
+    if (files.length > 0) {
+        files.filter(file => file.endsWith(".gradle")).forEach(file => {
+            const fileContent = readFileSync(join(location, file), "utf-8");
+            // Adds compatibility to new gradle 7
+            writeFileSync(join(location, file), fileContent.replace("compile(", "implementation("));
+        });
+    }
 }
